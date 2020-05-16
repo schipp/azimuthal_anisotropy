2defaee7b943494c36f695cc076100538e20dc1f
schipp <s.schippkus@icloud.com>
2020-05-16 18:11:52 +0200

    first implementation of entire programe logic

--- main.py
+++ main.py
@@ -1,287 +1,234 @@
-def get_cell_corners(cell:list, width:float, height:float, rel_loc='mid') -> list:
+import numpy as np
+from itertools import combinations
+from lib.geometry import generate_grid, get_ray_lengths_within_cells, get_distance
+from lib.anisotropy import aniso_parametrization, bin_az_measurements, fit_aniso, extract_aniso_results
+import logging
+
+def generate_synthetic_example(n_stations=65, lat_lims=(45, 50), lon_lims=(10, 20), u_0=0, A=.1, phi_2=60, B=.01, phi_4=20, amplitude_noise=.05):
     """
-    Returns the corners of a cell (x1, x2, y1, y2).
-    
-    :param cell: A tuple of lat/Y,lon/X-coordinates of the cell
-    :param width: Width of the cell
-    :param height: Height of the cell
-    :param rel_loc: Which position of the cell does `cell` indicate: center (mid)
-    :returns: y1, y2, x1, x2
+    Helper function to generate a simple synthetic example.
+    Constant anisotropy in entire region.
 
-    :Example:
-
+    :param n_stations: Number of stations in study area
+    :param lat_lims: Latitude limits of study area
+    :param lon_lims: Longitude limits of study area
+    :param A: Amplitude of 2Theta-term
+    :param phi_2: Phase of 2Theta-term
+    :param B: Amplitude of 4Theta-term
+    :param phi_4: Phase of 4Theta-term
+    :param u_0: Isotropic velocity
+    :returns: station pair locations  and velocities
     """
-    y, x = cell
-
-    if rel_loc == 'mid':
-        x1 = x - width/2
-        x2 = x + width/2
-        y1 = y - height/2
-        y2 = y + height/2
-    elif rel_loc == 'ul':
-        x1 = x
-        x2 = x + width
-        y1 = y - height
-        y2 = y
-    else:
-        raise AttributeError('Invalid rel_loc. Supported: mid, ul')
-    
-    corners = [
-        [y2, x1],
-        [y2, x2],
-        [y1, x2],
-        [y1, x1],
-        ]
-
-    return corners
-
-
-def get_cell_borders(corners:list) -> list:
-    """
-    Computes the start- and end-points of the four lines delimiting a cell.
-    
-    :param cell_corners: List of 4 tuples (corner-coordinates in Y,X)
-    :returns: List of 4 lists (= 4 cell-borders) containg 2 tuples (= start- and end-points in [Y, X])
-
-    :Example:
-
-    """
-
-    cell_borders = [
-        [corners[0], corners[1]],
-        [corners[1], corners[2]],
-        [corners[2], corners[3]],
-        [corners[3], corners[0]],
-        ]
-
-    return cell_borders
-
-def is_in_cell(point:list, corners:list) -> bool:
-    """
-    Checks if a point is within a cell.
-    
-    :param point: Tuple of lat/Y,lon/X-coordinates
-    :param corners: List of corner coordinates
-    :returns: Boolean whether point is within cell
-
-    :Example:
-
-    """    
-    y1, y2, x1, x2 = corners[2][0], corners[0][0], corners[0][1], corners[2][1]
-
-    if (y1 <= point[0] <= y2) and (x1 <= point[1] <= x2):
-        return True
-    return False
-
-def get_distance(point1:tuple, point2:tuple, coord_type='geographic') -> float:
-    """
-    Compute distance between two points
-    
-    :param point1: Tuple of lat/Y,lon/X-coordinates
-    :param point2: Tuple of lat/Y,lon/X-coordinates
-    :param coord_type: Type of coordinate system. 'cartesian' or 'geographic'
-    :returns: Distance between the points in meters
-
-    :Example:
-
-    """    
-
-    if coord_type == 'geographic':
-        from obspy.geodetics import gps2dist_azimuth
-        return gps2dist_azimuth(lat1=point1[0], lon1=point1[1], lat2=point2[0], lon2=point2[1])
-    elif coord_type == 'cartesian':
-        import numpy as np
-        return np.sqrt((np.array(point1) - np.array(point2))**2)
-    else:
-        raise AttributeError('coord_type invalid: Supported: cartesian or geographic')
-
-
-def get_intersection(line1:list, line2:list) -> list:
-    """
-    Check if line1 and line2 intersect and returns intersection-point coordinates as list.
-    Returns empty list, if no intersection point.
-    
-    :param line1: List of 2 tuples of lat/Y,lon/X-coordinates
-    :param line1: List of 2 tuples of lat/Y,lon/X-coordinates
-    :returns: Tuple of intersection-point coordinates [Y, X]. Empty list, if no intersection.
-
-    :Example:
-
-    """  
-
-    from shapely.geometry import LineString
-    l1 = LineString(((line1[0][1], line1[0][0]), (line1[1][1], line1[1][0])))
-    l2 = LineString(((line2[0][1], line2[0][0]), (line2[1][1], line2[1][0])))
-    if l1.intersects(l2):
-        point = l1.intersection(l2)
-        return [point.y, point.x]
-    return []
-
-
-
-def get_ray_lengths_within_cells(rays:list, cells:list, cell_width:float, cell_height:float, rel_loc='mid', coord_type='geographic') -> list:
-    """
-    Main logic of checking all cells for crossing rays and saving the lengths of each ray within cells.
-    
-    :param rays: List of tuples of tuples of station-pair lat/Y,lon/X-coordinates
-    :param cells: List of tuples of lat/Y,lon/X-coordinates of the cell
-    :param cell_width: Width of the cell
-    :param cell_height: Height of the cell
-    :param rel_loc: Which position of the cell does `cell` indicate: center (mid)
-    :param coord_type: Type of coordinate system. 'cartesian' or 'geographic'
-    :returns: List with length len(cells), each entry containing the ids and lenghts of all crossing rays for each cell.
-
-    :Example:
-
-    """
-    # pseudo-code (aka all functions are missing) example of how to determine if a ray crosses
-    # a cell and save the length of the segment within the cell (intended as weight later on)
-    #
-    # cells are a list of X,Y- or lon,lat-coordinates.
-    # rays are a list of ray-end-point (aka station-pair) coordinates. 
-
-    # path_lengths an empty list of lists same length as cells
-    ray_lengths_within_cells = [[] for _ in cells]
-    for cell, ray_lengths_within_cell in zip(cells, ray_lengths_within_cells):
-        cell_corners = get_cell_corners(cell=cell, width=cell_width, height=cell_height, rel_loc=rel_loc)
-        # determine the four lines that delimit the cell.
-        cell_borders = get_cell_borders(corners=cell_corners)
-        for ray_idx, ray in enumerate(rays):
-            intersection_points = []
-            for cell_border in cell_borders:
-                # check if and where the line segments intersect
-                intersection = get_intersection(line1=cell_border, line2=ray)
-                if intersection:
-                    intersection_points.append(intersection)
-            # no intersections
-            if len(intersection_points) == 0:
-                # both end-points within cell?
-                if is_in_cell(point=ray[0], corners=cell_corners) and is_in_cell(point=ray[1], corners=cell_corners):
-                    point1 = ray[0]
-                    point2 = ray[1]
-                else:
-                    # skip to next ray if end-points oustide of cell and no intersection
-                    continue
-            # 1 intersection: ray ends or starts in the cell
-            elif len(intersection_points) == 1:
-                point1 = intersection_points[0]
-                # which of the two ray-endpoints is in cell
-                if is_in_cell(point=ray[0], corners=cell_corners):
-                    point2 = ray[0]
-                if is_in_cell(point=ray[1], corners=cell_corners):
-                    point2 = ray[1]
-            # 2 intersections: ray crosses the cell
-            elif len(intersection_points) == 2:
-                point1 = intersection_points[0]
-                point2 = intersection_points[1]
-            else:
-                print('intersection_points should not be longer than 2 entries. Investigate!')
-                continue
-
-            len_in_cell = get_distance(point1=point1, point2=point2, coord_type=coord_type)
-
-            if len_in_cell[0] > 4000E3:
-                print(len(intersection_points), intersection_points)
-                print('')
-                print(ray[0])
-                print(ray[1])
-                print('')
-                print(point1)
-                print(point2)
-                print('')
-            ray_lengths_within_cell.append([ray_idx, len_in_cell])
-
-    return ray_lengths_within_cells
-
-if __name__ == '__main__':
-    # Run a random example (random distribution of stations within a grid)
-    import numpy as np
-    
-    # I use lat/lon coordinates for everything (i.e, Y/X)
-    # construct grid
-    from itertools import product, combinations
-    cell_width = .25
-    cell_height = .25
-    grid_x = np.arange(10, 20, cell_width)
-    grid_y = np.arange(40, 50, cell_height)
-    grid_points = list(product(grid_y, grid_x))
-
-    # construct rays
-    station_x = np.random.uniform(11, 19, [10])
-    station_y = np.random.uniform(41, 49, [10])
+    # random station distribution inside grid
+    station_x = np.random.uniform(lon_lims[0]-1, lon_lims[1]+1, [n_stations])
+    station_y = np.random.uniform(lat_lims[0]-1, lat_lims[1]+1, [n_stations])
     station_coords = list(zip(station_y, station_x))
     station_pairs = list(combinations(station_coords, 2))
 
+    # compute synthetic velocities for station pairs
+    vels = []
+    for sp in station_pairs:
+        dist, az, baz = get_distance(
+            point1=sp[0],
+            point2=sp[1],
+            coord_type='geographic')
+
+        vel_for_sp = aniso_parametrization(
+            x=np.deg2rad(np.min([az, baz])),
+            A=A,
+            B=B,
+            u_0=u_0,
+            phi_2=np.deg2rad(phi_2),
+            phi_4=np.deg2rad(phi_4)
+            )
+        
+        # add noise
+        vel_for_sp += np.random.uniform(-amplitude_noise, amplitude_noise)
+        
+        vels.append(vel_for_sp)
+
+    return station_pairs, vels
+
+if __name__ == '__main__':
+    # -- Synthetic or Real data?
+    do_synthetic_example = False
+    csv_file = 'synth.csv'
+    # -- Geometry of grid
+    # size of cells in decimal degrees
+    grid_point_cellsize_degree = 1.5
+    # overlap of cells from 0 to 1.
+    grid_point_overlap = .5
+    # -- Curve-fitting
+    # size of azimuth-bins in degrees
+    az_bin_size = 5
+
+    # define grid
+    lat_lims = [45, 50]
+    lon_lims = [10, 20]
+    grid_points, gp_meshgrid = generate_grid(
+        lats=lat_lims,
+        lons=lon_lims,
+        cellsize=grid_point_cellsize_degree,
+        grid_overlap=grid_point_overlap
+        )
+
+    # get station pair locations and velocities
+    if do_synthetic_example:
+        station_pairs, vels = generate_synthetic_example()
+        output_format = np.empty([len(station_pairs), 5])
+        station_pairs = np.array(station_pairs)
+        # output_format[:, 0:4] = station_pairs.reshape(station_pairs.shape[0], 4)
+        # output_format[:, 4] = np.array(vels)
+        # np.savetxt(csv_file, output_format, delimiter=',')
+
+    else:
+        real_data = np.loadtxt(csv_file, delimiter=',')
+        sps_1 = real_data[:, 0:2]
+        sps_2 = real_data[:, 2:4]
+        station_pairs = np.array([[sp1, sp2] for sp1, sp2 in zip(sps_1, sps_2)])
+        vels = real_data[:, 4]
+    
+    # get lengths of rays within each cell
     ray_lengths_within_cells = get_ray_lengths_within_cells(
         rays=station_pairs,
         cells=grid_points,
-        cell_width=cell_width,
-        cell_height=cell_height,
+        cell_width=grid_point_cellsize_degree,
+        cell_height=grid_point_cellsize_degree,
         rel_loc='mid',
         coord_type='geographic')
-    
-    # path_density is simply the total amount of computed segment-lengths within each cell
-    path_density = [len(_) for _ in ray_lengths_within_cells]
 
-    # plot example
+    # analyze anisotropy for each cell
+    fast_directions = np.zeros(len(grid_points))
+    fast_directions[:] = np.nan
+    fast_directions_errors = np.zeros(len(grid_points))
+    fast_directions_errors[:] = np.nan
+    fast_amps_A = np.zeros(len(grid_points))
+    fast_amps_A[:] = np.nan
+    fast_amps_A_errors = np.zeros(len(grid_points))
+    fast_amps_A_errors[:] = np.nan
+
+    for idx, (cell, ray_params) in enumerate(zip(grid_points, ray_lengths_within_cells)):
+        # cell has no crossing rays
+        if len(ray_params) == 0:
+            continue
+        # extract ray parameters for cell
+        ray_params = np.array(ray_params)
+        ray_idxs = ray_params[:, 0]
+        ray_geometry_in_cell = ray_params[:, 1]
+        ray_lens = [_[0] for _ in ray_geometry_in_cell]
+        ray_azs = [_[1] for _ in ray_geometry_in_cell]
+        ray_bazs = [_[2] for _ in ray_geometry_in_cell]
+
+        current_vels = np.array(vels)[list(ray_idxs)]
+        current_azis = np.min(np.array([ray_azs, ray_bazs]), axis=0)
+
+        # bin velocity measurements into azimuth-bins
+        az_bins, vel_bin_medians, vel_bin_stds = bin_az_measurements(
+            vels=current_vels,
+            bazs=current_azis,
+            az_step=az_bin_size
+            )
+
+        # fit anisotropic parameters
+        if len(vel_bin_medians) < 8:
+            logging.info(f"GP {cell[0]:0.2f}/{cell[1]:0.2f}: not enough azimuth-bins w/ data ({len(vel_bin_medians)}) to resolve anisotropy.")
+            continue
+
+        params, params_covariance = fit_aniso(
+            az_bins=az_bins,
+            az_step=az_bin_size,
+            vel_bin_medians=vel_bin_medians,
+            vel_bin_stds=vel_bin_stds
+            )
+
+        phi_2, phi_2_std, rel_A, rel_A_std = extract_aniso_results(params, params_covariance)
+
+        # sanity checks, these will trigger if unstable or overfitted
+        if np.isinf(phi_2_std):
+            logging.info(f"GP {cell[0]:0.2f}/{cell[1]:0.2f}: sanity check #1 failed. phi_2_std == ∞")
+            continue
+        if phi_2_std < 0.01:
+            logging.info(f"GP {cell[0]:0.2f}/{cell[1]:0.2f}: sanity check #2 failed. phi_2_std < 0.01")
+            continue
+
+        fast_directions[idx] = phi_2
+        fast_directions_errors[idx] = phi_2_std
+        fast_amps_A[idx] = np.abs(rel_A)
+        fast_amps_A_errors[idx] = rel_A_std
+
+        ## cell fit visualization
+        # import pylab as plt
+        # fig, ax = plt.subplots(1, 1)
+        # test_az = np.arange(0, 180+1, 1)
+        # test_vels = aniso_parametrization(x=test_az, A=rel_A, B=rel_B, u_0=0, phi_2=phi_2, phi_4=phi_4)
+        # ax.scatter(current_azis, current_vels, c='#CCCCCC')
+        # ax.errorbar(az_bins+az_bin_size/2, vel_bin_medians, fmt='o', yerr=vel_bin_stds, capsize=4, c='k')
+        # ax.plot(test_az, test_vels)
+        # ax.set_xlim(0, 180)
+        # plt.show(fig)
+        # plt.close(fig)
+        # break
+
+    # visualize results
     import pylab as plt
-    from matplotlib.colors import Normalize
-
-    norm = Normalize(vmin=0, vmax=max(path_density))
-
     fig, axs = plt.subplots(1, 2)
+    
+    # transform radial to cartesian coordinates
+    x_comp = np.multiply(fast_amps_A, np.sin(np.radians(fast_directions)))
+    y_comp = np.multiply(fast_amps_A, np.cos(np.radians(fast_directions)))
+
+    xx, yy = gp_meshgrid
+
     ax = axs[0]
     ax.set_aspect('equal')
+    ax.scatter(
+        [station_pairs[:, 0, 1], station_pairs[:, 1, 1]], 
+        [station_pairs[:, 0, 0], station_pairs[:, 1, 0]],
+        marker='^')
 
-    gps = np.array(grid_points)
-    xx, yy = np.meshgrid(grid_x, grid_y)
-
-    pd = np.array(path_density).reshape(len(grid_x), len(grid_y))
-    img = ax.pcolormesh(xx-cell_width/2, yy-cell_height/2, pd, norm=norm, cmap='viridis_r')
+    Q = ax.quiver(
+        xx,
+        yy,
+        x_comp.T,
+        y_comp.T,
+        color='#d62728',
+        width=0.008,
+        headlength=0,
+        headwidth=0,
+        headaxislength=0,
+        scale=50000,
+        pivot='mid'
+        )
     
-    scs = np.array(station_coords)
-    ax.scatter(scs[:,1], scs[:,0], marker='^', facecolors='k', edgecolors='w')
+    ax.set_xlim(lon_lims)
+    ax.set_ylim(lat_lims)
     
-    x0, y0, w, h = ax.get_position().bounds
-    # cb_ax = fig.add_axes([x0+w+.01*w, y0, .02*w, h])
-    cb_ax = fig.add_axes([x0+.025*w, y0+.025*h, .02*w, .95*h])
-    plt.colorbar(img, cax=cb_ax)
-    
-
-    # visualize the densest cell to check if everythings working
-    idx_dense = np.argmax(path_density)
-    gp_dense = grid_points[idx_dense]
-    rayidx_dense = np.array(ray_lengths_within_cells[idx_dense])[:,0]
-    ray_params = np.array(ray_lengths_within_cells[idx_dense])[:,1]
-    
-    corners_of_dense = get_cell_corners(cell=gp_dense, width=cell_width, height=cell_height)
-    borders_of_dense = get_cell_borders(corners=corners_of_dense)
-    for border in borders_of_dense:
-        ax.plot((border[0][1], border[1][1]), (border[0][0], border[1][0]), c='red', lw=2)
-
-
     ax = axs[1]
     ax.set_aspect('equal')
-    for ray_idx, ray_param in zip(rayidx_dense, ray_params):
-        sta1_x = station_pairs[ray_idx][0][1]
-        sta1_y = station_pairs[ray_idx][0][0]
-        sta2_x = station_pairs[ray_idx][1][1]
-        sta2_y = station_pairs[ray_idx][1][0]
-        ax.plot(
-            [sta1_x, sta2_x], [sta1_y, sta2_y],
-            label=f"{ray_param[0]/1000:0.1f}km / {np.min(ray_param[1:]):0.1f}°"
-            )
-    ax.scatter(scs[:,1], scs[:,0], s=80, marker='^', facecolors='k', edgecolors='w', zorder=10)
-    ax.set_xlim(gp_dense[1]-cell_width/2, gp_dense[1]+cell_width/2)
-    ax.set_ylim(gp_dense[0]-cell_height/2, gp_dense[0]+cell_height/2)
-    ax.set_xticks((gp_dense[1]-cell_width/2, gp_dense[1]+cell_width/2))
-    ax.set_yticks((gp_dense[0]-cell_height/2, gp_dense[0]+cell_height/2))
-    ax.spines['bottom'].set_color('red')
-    ax.spines['top'].set_color('red') 
-    ax.spines['right'].set_color('red')
-    ax.spines['left'].set_color('red')
+    ax.scatter(
+        [station_pairs[:, 0, 1], station_pairs[:, 1, 1]], 
+        [station_pairs[:, 0, 0], station_pairs[:, 1, 0]],
+        marker='^')
 
-    ax.legend(loc=2, bbox_to_anchor=(1, 1), fontsize=6)
+    x_comp_fixed_len = np.multiply(1, np.sin(np.radians(fast_directions)))
+    y_comp_fixed_len = np.multiply(1, np.cos(np.radians(fast_directions)))
+
+    Q = ax.quiver(
+        xx,
+        yy,
+        x_comp_fixed_len.T,
+        y_comp_fixed_len.T,
+        color='#d62728',
+        width=0.008,
+        headlength=0,
+        headwidth=0,
+        headaxislength=0,
+        scale=10,
+        pivot='mid'
+        )
+    
+    ax.set_xlim(lon_lims)
+    ax.set_ylim(lat_lims)
 
     plt.show(fig)
     plt.close(fig)
